7384de24099bce94687d32c0193c3091014a5202
trungcheng <trungs1bmt@gmail.com>
2018-06-19 17:47:03 +0700

    api route

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -68,10 +68,10 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         // 'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        // 'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         // 'can' => \Illuminate\Auth\Middleware\Authorize::class,
         // 'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        // 'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'admin.auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'admin.guest' => \App\Http\Middleware\Admin\RedirectIfAuthenticated::class,
         'admin.values' => \App\Http\Middleware\Admin\SetDefaultValues::class,
