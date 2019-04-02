51c433985455c45c4378c225e1188681935c1271
trungcheng <trungs1bmt@gmail.com>
2019-04-02 17:44:15 +0700

    fix

--- app/Http/Controllers/User/ProductController.php
+++ app/Http/Controllers/User/ProductController.php
@@ -173,7 +173,7 @@ class ProductController extends Controller
                 $products->orderBy('created_at', 'desc');
             }
 
-            $results = $products->paginate(12);
+            $results = $products->get();
 
             return view(($sex != 'c') ? 'pages.user.product.store' : 'pages.user.product.brand-store', [
                 'sex' => $sex,
