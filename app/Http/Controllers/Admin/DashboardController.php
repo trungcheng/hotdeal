d6a0e5e01e5bc305bad3437a908d2a72eec12637
trungcheng <trungs1bmt@gmail.com>
2019-08-22 10:32:32 +0700

    update dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\History;
 use App\Models\User;
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
@@ -14,8 +17,17 @@ class DashboardController extends Controller
 
     public function index()
     {
+    	$userCount = User::where('type', 0)->count();
+    	$memberCount = User::where('type', 1)->count();
+    	$categoryCount = Category::count();
+    	$voteCount = History::whereDate('created_at', Carbon::today())
+    		->selectRaw('sum(vote_count) as vote')->get();
+
         return view('pages.admin.dashboard.index', [
-            
+            'userCount' => $userCount,
+            'memberCount' => $memberCount,
+            'categoryCount' => $categoryCount,
+            'voteCount' => $voteCount
         ]);
     }
 }
--- resources/views/pages/admin/dashboard/index.blade.php
+++ resources/views/pages/admin/dashboard/index.blade.php
@@ -20,72 +20,31 @@
     	<section class="content container-fluid">
 
             <div class="row">
-                <div class="col-md-3 col-sm-6 col-xs-12">
-                    <div class="info-box">
-                        <span class="info-box-icon bg-aqua"><i class="fa fa-dollar"></i></span>
-                        <div class="info-box-content">
-                            <span class="info-box-text">Tổng doanh thu</span>
-                            <span class="info-box-number">90,000,000đ<small></small></span>
+                <!-- ./col -->
+                <div class="col-lg-3 col-xs-6">
+                    <!-- small box -->
+                    <div class="small-box bg-yellow">
+                        <div class="inner">
+                            <h3>{{ $userCount }}</h3>
+                            <p>Tổng số thành viên</p>
                         </div>
-                        <!-- /.info-box-content -->
-                    </div>
-                    <!-- /.info-box -->
-                </div>
-                <!-- /.col -->
-                <div class="col-md-3 col-sm-6 col-xs-12">
-                    <div class="info-box">
-                        <span class="info-box-icon bg-red"><i class="fa fa-dollar"></i></span>
-                        <div class="info-box-content">
-                            <span class="info-box-text">Doanh thu hôm nay</span>
-                            <span class="info-box-number">410,410đ</span>
+                        <div class="icon">
+                            <i class="ion ion-person"></i>
                         </div>
-                        <!-- /.info-box-content -->
+                        <a href="{{ route('users') }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
                     </div>
-                    <!-- /.info-box -->
                 </div>
-                <!-- /.col -->
-
-                <!-- fix for small devices only -->
-                <div class="clearfix visible-sm-block"></div>
-
-                <div class="col-md-3 col-sm-6 col-xs-12">
-                    <div class="info-box">
-                        <span class="info-box-icon bg-green"><i class="fa fa-dollar"></i></span>
-                        <div class="info-box-content">
-                            <span class="info-box-text">Doanh thu tháng</span>
-                            <span class="info-box-number">12,000,000đ</span>
-                        </div>
-                        <!-- /.info-box-content -->
-                    </div>
-                    <!-- /.info-box -->
-                </div>
-                <!-- /.col -->
-                <div class="col-md-3 col-sm-6 col-xs-12">
-                    <div class="info-box">
-                        <span class="info-box-icon bg-yellow"><i class="fa fa-dollar"></i></span>
-                        <div class="info-box-content">
-                            <span class="info-box-text">Doanh thu năm</span>
-                            <span class="info-box-number">202,000,000đ</span>
-                        </div>
-                        <!-- /.info-box-content -->
-                    </div>
-                    <!-- /.info-box -->
-                </div>
-                <!-- /.col -->
-            </div>
-
-            <div class="row">
                 <div class="col-lg-3 col-xs-6">
                     <!-- small box -->
                     <div class="small-box bg-aqua">
                         <div class="inner">
-                            <h3>150</h3>
-                            <p>Đơn hàng mới</p>
+                            <h3>{{ $memberCount }}</h3>
+                            <p>Tổng số nhân vật</p>
                         </div>
                         <div class="icon">
-                            <i class="ion ion-bag"></i>
+                            <i class="ion ion-person"></i>
                         </div>
-                        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                        <a href="{{ route('members') }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
@@ -93,27 +52,13 @@
                     <!-- small box -->
                     <div class="small-box bg-green">
                         <div class="inner">
-                            <h3>53<sup style="font-size: 20px"></sup></h3>
-                            <p>Sản phẩm hiện có</p>
+                            <h3>{{ $categoryCount }}<sup style="font-size: 20px"></sup></h3>
+                            <p>Tổng số khối (Phòng/Ban)</p>
                         </div>
                         <div class="icon">
                             <i class="ion ion-stats-bars"></i>
                         </div>
-                        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-                    </div>
-                </div>
-                <!-- ./col -->
-                <div class="col-lg-3 col-xs-6">
-                    <!-- small box -->
-                    <div class="small-box bg-yellow">
-                        <div class="inner">
-                            <h3>44</h3>
-                            <p>Thành viên mới</p>
-                        </div>
-                        <div class="icon">
-                            <i class="ion ion-person-add"></i>
-                        </div>
-                        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                        <a href="{{ route('categories') }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
@@ -121,13 +66,13 @@
                     <!-- small box -->
                     <div class="small-box bg-red">
                         <div class="inner">
-                            <h3>65</h3>
-                            <p>Khách truy cập hôm nay</p>
+                            <h3>{{ ($voteCount[0]['vote']) ? $voteCount[0]['vote'] : 0 }}</h3>
+                            <p>Tổng lượt bình chọn hôm nay</p>
                         </div>
                         <div class="icon">
                             <i class="ion ion-pie-graph"></i>
                         </div>
-                        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                        <a href="{{ route('statistical') }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
