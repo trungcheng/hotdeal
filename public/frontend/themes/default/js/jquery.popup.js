bf0999903827d4fcf8a188cc9c08dc425de293e4
trungcheng <trungs1bmt@gmail.com>
2019-12-02 23:28:11 +0700

    initial

---
+++ public/frontend/themes/default/js/jquery.popup.js
@@ -0,0 +1,112 @@
+//please contact me giaiphapthuonghieu.org@gmail.com
+//website http://giaiphapthuonghieu.vn or http://faceseo.vn
+(function($) {
+    $.fn.vinastar = function(options) {
+        
+        
+        var defaults = {  
+	    	animation: 'fadeAndPop', //fade, fadeAndPop, none
+		    animationspeed: 300, //how fast animtions are
+		    closeonbackgroundclick: true, //if you click background will modal close?
+		    dismissmodalclass: 'close-vinastar-modal' //the class of a button or element that will close an open modal
+    	}; 
+
+        var options = $.extend({}, defaults, options); 
+	
+        return this.each(function() {
+        	var modal = $(this),
+        		topMeasure  = parseInt(modal.css('top')),
+				topOffset = modal.height() + topMeasure,
+          		locked = false,
+				modalBG = $('.vinastar-modal-bg');
+
+			if(modalBG.length == 0) {
+				modalBG = $('<div class="vinastar-modal-bg" />').insertAfter(modal);
+			}		    
+			modal.bind('vinastar:open', function () {
+			  modalBG.unbind('click.modalEvent');
+				$('.' + options.dismissmodalclass).unbind('click.modalEvent');
+				if(!locked) {
+					lockModal();
+					if(options.animation == "fadeAndPop") {
+						modal.css({'top': $(document).scrollTop()-topOffset, 'opacity' : 0, 'visibility' : 'visible'});
+						modalBG.fadeIn(options.animationspeed/2);
+						modal.delay(options.animationspeed/2).animate({
+							"top": $(document).scrollTop()+topMeasure + 'px',
+							"opacity" : 1
+						}, options.animationspeed,unlockModal());					
+					}
+					if(options.animation == "fade") {
+						modal.css({'opacity' : 0, 'visibility' : 'visible', 'top': $(document).scrollTop()+topMeasure});
+						modalBG.fadeIn(options.animationspeed/2);
+						modal.delay(options.animationspeed/2).animate({
+							"opacity" : 1
+						}, options.animationspeed,unlockModal());					
+					} 
+					if(options.animation == "none") {
+						modal.css({'visibility' : 'visible', 'top':$(document).scrollTop()+topMeasure});
+						modalBG.css({"display":"block"});	
+						unlockModal()				
+					}
+				}
+				modal.unbind('vinastar:open');
+			}); 	
+
+			modal.bind('vinastar:close', function () {
+			  if(!locked) {
+					lockModal();
+					if(options.animation == "fadeAndPop") {
+						modalBG.delay(options.animationspeed).fadeOut(options.animationspeed);
+						modal.animate({
+							"top":  $(document).scrollTop()-topOffset + 'px',
+							"opacity" : 0
+						}, options.animationspeed/2, function() {
+							modal.css({'top':topMeasure, 'opacity' : 1, 'visibility' : 'hidden'});
+							unlockModal();
+						});					
+					}  	
+					if(options.animation == "fade") {
+						modalBG.delay(options.animationspeed).fadeOut(options.animationspeed);
+						modal.animate({
+							"opacity" : 0
+						}, options.animationspeed, function() {
+							modal.css({'opacity' : 1, 'visibility' : 'hidden', 'top' : topMeasure});
+							unlockModal();
+						});					
+					}  	
+					if(options.animation == "none") {
+						modal.css({'visibility' : 'hidden', 'top' : topMeasure});
+						modalBG.css({'display' : 'none'});	
+					}		
+				}
+				modal.unbind('vinastar:close');
+			});     
+   	
+    	modal.trigger('vinastar:open')
+			
+			//Close Modal Listeners
+			var closeButton = $('.' + options.dismissmodalclass).bind('click.modalEvent', function () {
+			  modal.trigger('vinastar:close')
+			});
+			
+			if(options.closeonbackgroundclick) {
+				modalBG.css({"cursor":"pointer"})
+				modalBG.bind('click.modalEvent', function () {
+				  modal.trigger('vinastar:close')
+				});
+			}
+			$('body').keyup(function(e) {
+        		if(e.which===27){ modal.trigger('vinastar:close'); } // 27 is the keycode for the Escape key
+			});
+			
+			function unlockModal() { 
+				locked = false;
+			}
+			function lockModal() {
+				locked = true;
+			}	
+			
+        });//each call
+    }//orbit plugin call
+})(jQuery);
+        
--- resources/views/pages/user/home/index.blade.php
+++ resources/views/pages/user/home/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.user.master')
 
-@section('page')Trang chủ | Vincom Retail
+@section('page')Trang chủ
 @stop
 
 @section('description')
@@ -35,85 +35,11 @@
 @stop
 
 @section('content')
-    <div class="wrapper">
-		<div class="pd-t-60 pd-b-60">
-			<div class="container box-top">
-				<div class="row">
-					<div class="col-lg-5 col-xs-12">
-						<div class="box-slogan">
-							<img src="{{ asset('frontend/images/slogan.png') }}">
-						</div>
-						<div class="txt-slogan mg-t-50">
-							<h3>Bình chọn</h3>
-							<h4>Đại sứ trái tim</h4>
-							<p>Bình chọn 15 gương mặt đại sứ dịch vụ tiêu biểu nhận danh hiệu "Đại sứ trái tim"</p>
-
-							@if($round->visible_menu == 1)
-							<a style="margin-left: 0;" class="btn-vote transition" href="#">Bình chọn ngay</a>
-							@endif
-						</div>
-					</div>
-					<div class="col-lg-7 col-xs-12 inner-bg">
-						<img class="bg-slogan" src="{{ $setting->image_home_page }}">
-					</div>
-				</div>
-			</div>
-
-			<div class="box-button mg-t-50 pd-t-30 pd-b-20">
-				<div class="container txt-center">
-					<h3>Bình chọn ngay cho những gương mặt tiêu biểu đến từ các khối, phòng ban</h3>
-					@if($round->visible_menu == 1)
-						<?php $i = 0; ?>
-						@foreach ($categories as $cate)
-							<?php $i++; ?>
-							<div class="box-btn-vote">
-								<a class="btn-vote btn-vote-2 wfm transition" onclick="open_group({{ $i }});">{{ $cate['name'] }}</a>
-	                        @if (isset($cate['childrens']))
-	                        	<ul class="list-child list-{{ $i }} transition">
-	                        		@foreach ($cate['childrens'] as $child)
-	                        			<li><a class="transition" href="/{{ $cate['slug'] }}/{{ $child['slug'] }}.html">{{ $child['name'] }}</a></li>
-	                                @endforeach
-								</ul>
-	                        @else
-	                        <a class="btn-vote btn-vote-2 wfm transition" onclick="open_group({{ $i }});">{{ $cate['name'] }}</a>
-	                        @endif
-	                        </div>
-	                    @endforeach
-                    @else
-	                    <a style="margin-left: 0;" class="btn-vote transition" href="/vong-thi/{{ $round->slug }}.html">Bình chọn {{ $round->name }}</a>
-	                @endif
-				</div>
-			</div>
-
-			<div class="container">
-				<div class="content-home">
-					<div class="img-content hidden-mobile"><img src="{{ asset('frontend/images/vn.png') }}"> </div>
-					{!! $setting->content_home_page !!}
-				</div>
-			</div>
-		</div>
-	</div>
+    
 @stop
 
 @section('pageJs')
 	<script type="text/javascript">
-	   $(document).ready(function(){
-	        cal_img();
-	    });
-
-	   	function cal_img() {
-	   		var width = window.innerWidth;
-			var divWidth = document.getElementsByClassName("box-top")[0].clientWidth;
-			var boxbg = document.getElementsByClassName("inner-bg")[0].clientWidth;
-			var cal = (width - divWidth)/2;
-	        $('.bg-slogan').css('max-width', (boxbg+cal)-5);
-
-	        var height = document.getElementsByClassName("bg-slogan")[0].clientHeight;
-	        $('.inner-bg').css('height', height+10);
-	   	}
-
-	   	window.onresize = function(event) {
-		    cal_img();
-		};
+	   	
 	</script>
 @stop
