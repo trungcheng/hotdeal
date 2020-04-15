2fb9e180d8595d0b2e712bd9ee0cee760f86cba1
trungcheng <trungs1bmt@gmail.com>
2020-04-15 21:23:25 +0700

    fix

--- resources/views/layouts/user/master.blade.php
+++ resources/views/layouts/user/master.blade.php
@@ -43,6 +43,15 @@
     @section('pageCss')
     @show
     {!! ($setting != '') ? $setting->fb_pixel_code : '' !!}
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-163726178-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-163726178-1');
+    </script>
 </head>
 
 <body>
